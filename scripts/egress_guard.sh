c2a18f3aadc194dd0c345d78750e3f6f9e56737e
Chiara Berti 13 <102581645+chiaraberti13@users.noreply.github.com>
2026-09-26 09:27:44 +0200

    security: include generic TCP profile in egress guard

--- scripts/egress_guard.sh
+++ scripts/egress_guard.sh
@@ -51,6 +51,7 @@ subnets=(
   "$(resolve_subnet AEGIS_REDIS_EXPOSURE_SUBNET 172.30.105.0/24)"
   "$(resolve_subnet AEGIS_MYSQL_EXPOSURE_SUBNET 172.30.106.0/24)"
   "$(resolve_subnet AEGIS_SMB_EXPOSURE_SUBNET 172.30.107.0/24)"
+  "$(resolve_subnet AEGIS_GENERIC_EXPOSURE_SUBNET 172.30.108.0/24)"
 )
 
 validate_subnets() {
